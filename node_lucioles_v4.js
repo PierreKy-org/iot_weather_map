69cc554e948138573f7bb93ba3f1905755186d7a
Zorrino <pierrekouyoumdjian@hotmail.fr>
2022-01-30 19:46:19 +0100

    modif

--- node_lucioles_v4.js
+++ node_lucioles_v4.js
@@ -200,7 +200,7 @@ app.use(function (request, response, next) {
 
 // Route / => Le node renvoie la page HTML affichant les charts
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/Leaflet_esp/index.html"));
 });
 
 // The request contains the name of the targeted ESP !
